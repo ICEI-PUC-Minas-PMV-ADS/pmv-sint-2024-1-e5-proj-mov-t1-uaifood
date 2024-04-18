c7d104e2af8a5283268c2cbf6d0ac177bc631934
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2024-04-18 00:00:22 -0300

    get added in all files

--- app/backend/app.js
+++ app/backend/app.js
@@ -2,19 +2,27 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv/config');
-const api = process.env.API_URL;
 
-const productsRouter = require('./routers/products');
-
-// middleware
+// Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// routers
-app.use(`${api}/produtos`, productsRouter);
+// Routes
+const categoriesRoutes = require('./routers/categories');
+const productsRoutes = require('./routers/products');
+const usersRoutes = require('./routers/users');
+const ordersRoutes = require('./routers/orders');
 
+const api = process.env.API_URL;
+
+app.use(`${api}/categorias`, categoriesRoutes);
+app.use(`${api}/produtos`, productsRoutes);
+app.use(`${api}/usuarios`, usersRoutes);
+app.use(`${api}/pedidos`, ordersRoutes);
+
+// Database
 mongoose.connect(process.env.CONNECTION_STRING, {
   dbName: 'uaifood-database'
 })
@@ -25,6 +33,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
   console.log(err);
 })
 
+// Server
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
 });
