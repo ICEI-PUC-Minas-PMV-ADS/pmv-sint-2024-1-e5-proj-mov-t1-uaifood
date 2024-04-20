4217f08c3d8e6d8a86e2e84c48ad9e0e2ffa5654
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2024-04-20 19:25:41 -0300

    get destaque products

--- app/backend/routers/products.js
+++ app/backend/routers/products.js
@@ -102,5 +102,23 @@ router.get(`/get/estatistica`, async (req, res) => {
   });
 });
 
+router.get(`/get/destaque/:count`, async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  const products = await Product.find({ isFeatured: true }).limit(+count);
+
+  if (!products) {
+      res.status(500).json({ success: false });
+  }
+  res.send(products);
+});
+
+router.get(`/get/destaque`, async (req, res) => {
+  const products = await Product.find({ isFeatured: true });
+
+  if (!products) {
+      res.status(500).json({ success: false });
+  }
+  res.send(products);
+});
 
 module.exports = router;
