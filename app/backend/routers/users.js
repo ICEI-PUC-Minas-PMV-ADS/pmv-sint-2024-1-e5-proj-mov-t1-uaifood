dcc4119cf6757c611f56ebc8671859c441e5dfcf
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2024-04-20 22:33:27 -0300

    excluding password hash

--- app/backend/routers/users.js
+++ app/backend/routers/users.js
@@ -4,12 +4,21 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select('-passwordHash');
 
   if(!userList) {
     res.status(500).json({ success: false });
   }
   res.send(userList);
+});
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id).select('-passwordHash');
+
+  if(!user) {
+    res.status(500).json({ message: 'O usuário não foi encontrada!' });
+  }
+  res.send(user);
 });
 
 router.post('/', async (req, res)=>{
