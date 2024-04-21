e95729c3b27547639a657e09ddd1ca50ccee4274
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2024-04-21 02:23:10 -0300

    adding put method to users

--- app/backend/routers/users.js
+++ app/backend/routers/users.js
@@ -45,6 +45,39 @@ router.post('/', async (req, res)=>{
   res.send(user);
 });
 
+router.put('/:id',async (req, res)=> {
+
+  const userExist = await User.findById(req.params.id);
+  let newPassword
+  if(req.body.passwordHash) {
+      newPassword = bcrypt.hashSync(req.body.passwordHash, 10)
+  } else {
+      newPassword = userExist.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+          name: req.body.name,
+          email: req.body.email,
+          passwordHash: newPassword,
+          phone: req.body.phone,
+          isAdmin: req.body.isAdmin,
+          street: req.body.street,
+          apartment: req.body.apartment,
+          zip: req.body.zip,
+          city: req.body.city,
+          country: req.body.country,
+      },
+      { new: true}
+  )
+
+  if(!user)
+  return res.status(400).send('O usuário não pode ser criado!')
+
+  res.send(user);
+})
+
 router.post('/login', async (req, res) => {
   const user = await User.findOne({ email: req.body.email});
   const secret = process.env.secret;
@@ -68,6 +101,5 @@ router.post('/login', async (req, res) => {
     res.status(400).send('Senha incorreta!')
   }
 });
-
 
 module.exports = router;
